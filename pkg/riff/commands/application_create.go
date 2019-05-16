90442421aab6cdbca36d11794dfb8af2eec46d29
markfisher <mfisher@pivotal.io>
2019-05-16 15:40:35 -0400

    wip for application create

--- pkg/riff/commands/application_create.go
+++ pkg/riff/commands/application_create.go
@@ -22,15 +22,65 @@ import (
 
 	"github.com/projectriff/riff/pkg/cli"
 	"github.com/spf13/cobra"
+	"k8s.io/apimachinery/pkg/api/resource"
+	buildv1alpha1 "github.com/projectriff/system/pkg/apis/build/v1alpha1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 type ApplicationCreateOptions struct {
-	Namespace string
+	cli.ResourceOptions
+
+	Image     string
+	CacheSize string
+
+	LocalPath   string
+	GitRepo     string
+	GitRevision string
+	SubPath     string
 }
 
 func (opts *ApplicationCreateOptions) Validate(ctx context.Context) *cli.FieldError {
-	// TODO implement
-	return nil
+	errs := &cli.FieldError{}
+
+	errs = errs.Also(opts.ResourceOptions.Validate(ctx))
+
+	if opts.Image == "" {
+		errs = errs.Also(cli.ErrMissingField("image"))
+	} else if false {
+		// TODO validate image
+	}
+
+	if opts.CacheSize != "" {
+		// must parse as a resource quantity
+		if _, err := resource.ParseQuantity(opts.CacheSize); err != nil {
+			errs = errs.Also(cli.ErrInvalidValue(opts.CacheSize, "cache-size"))
+		}
+	}
+
+	// git-repo and local-path are mutually exclusive
+	if opts.GitRepo == "" && opts.LocalPath == "" {
+		errs = errs.Also(cli.ErrMissingOneOf("git-repo", "local-path"))
+	} else if opts.GitRepo != "" && opts.LocalPath != "" {
+		errs = errs.Also(cli.ErrMultipleOneOf("git-repo", "local-path"))
+	}
+
+	// git-revision is required for git-repo
+	if opts.GitRepo != "" && opts.GitRevision == "" {
+		errs = errs.Also(cli.ErrMissingField("git-revision"))
+	}
+
+	if opts.LocalPath != "" {
+		if opts.SubPath != "" {
+			// sub-path cannot be used with local-path
+			errs = errs.Also(cli.ErrDisallowedFields("sub-path"))
+		}
+		if opts.CacheSize != "" {
+			// cache-size cannot be used with local-path
+			errs = errs.Also(cli.ErrDisallowedFields("cache-size"))
+		}
+	}
+
+	return errs
 }
 
 func NewApplicationCreateCommand(c *cli.Config) *cobra.Command {
@@ -40,14 +90,54 @@ func NewApplicationCreateCommand(c *cli.Config) *cobra.Command {
 		Use:     "create",
 		Short:   "<todo>",
 		Example: "<todo>",
-		Args:    cli.Args(),
+		Args: cli.Args(
+			cli.NameArg(&opts.Name),
+		),
 		PreRunE: cli.ValidateOptions(opts),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return fmt.Errorf("not implemented")
+			application := &buildv1alpha1.Application{
+				ObjectMeta: metav1.ObjectMeta{
+					Namespace: opts.Namespace,
+					Name:      opts.Name,
+				},
+				Spec: buildv1alpha1.ApplicationSpec{
+					Image: opts.Image,
+				},
+			}
+			if opts.CacheSize != "" {
+				quantity := resource.MustParse(opts.CacheSize)
+				application.Spec.CacheSize = &quantity
+			}
+			if opts.GitRepo != "" {
+				application.Spec.Source = buildv1alpha1.Source{
+					Git: &buildv1alpha1.GitSource{
+						URL:      opts.GitRepo,
+						Revision: opts.GitRevision,
+					},
+					SubPath: opts.SubPath,
+				}
+			}
+			if opts.LocalPath != "" {
+				// TODO implement
+				return fmt.Errorf("not implemented")
+			}
+
+			application, err := c.Build().Applications(opts.Namespace).Create(application)
+			if err != nil {
+				return err
+			}
+			fmt.Fprintf(cmd.OutOrStdout(), "Created application %q\n", application.Name)
+			return nil
 		},
 	}
 
 	cli.NamespaceFlag(cmd, c, &opts.Namespace)
+	cmd.Flags().StringVar(&opts.Image, "image", "", "<todo>")
+	cmd.Flags().StringVar(&opts.CacheSize, "cache-size", "", "<todo>")
+	cmd.Flags().StringVar(&opts.LocalPath, "local-path", "", "<todo>")
+	cmd.Flags().StringVar(&opts.GitRepo, "git-repo", "", "<todo>")
+	cmd.Flags().StringVar(&opts.GitRevision, "git-revision", "master", "<todo>")
+	cmd.Flags().StringVar(&opts.SubPath, "sub-path", "", "<todo>")
 
 	return cmd
 }
